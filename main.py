527f7b172c7b9a7696d2e938f8162423917c098e
Arcadievich <user_274@mail.ru>
2025-08-01 11:01:55 +0300

    Fixed the URL and request verification

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ import requests
 
 def main():
     places = ['svo', 'london', 'cherepovets']
-    url_template = 'https://wttr.in/{0}?MnqT&lang=ru'
+    url_template = 'https://wttr.in/{0}'
+    payload = {
+        'M': '',
+        'n': '',
+        'q': '',
+        'T': '',
+        'lang': 'ru',
+    }
 
     for place in places:
         url = url_template.format(place)
-        response = requests.get(url)
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
         print(response.text)
 
 
